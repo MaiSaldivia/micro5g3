8fd3cbe2295c94d24300de4b703d04e13955cd9e
Maicol Antonio Saldivia Silva <mai.saldivia@duocuc.cl>
2025-05-26 18:41:02 -0400

    Primer commit del microservicio de inventario

---
+++ micro5g3/src/test/java/com/micro5/micro5g3/Micro5g3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro5.micro5g3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro5g3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
